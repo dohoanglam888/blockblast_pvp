09e27348cb490b43ecf70cf9683cb7da824d9ce2
Do Hoang Lam <96128786+dohoanglam888@users.noreply.github.com>
2026-05-10 00:55:24 +0700

    Update board.js

--- src/game/board.js
+++ src/game/board.js
@@ -9,13 +9,13 @@ export function canPlace(board, shape, row, col) {
     for (let x = 0; x < shape[y].length; x++) {
       if (!shape[y][x]) continue
 
-      const newRow = row + y
-      const newCol = col + x
+      const r = row + y
+      const c = col + x
 
       if (
-        newRow >= 8 ||
-        newCol >= 8 ||
-        board[newRow][newCol]
+        r >= 8 ||
+        c >= 8 ||
+        board[r][c]
       ) {
         return false
       }
@@ -25,43 +25,18 @@ export function canPlace(board, shape, row, col) {
   return true
 }
 
-export function placeShape(board, shape, row, col) {
+export function placeShape(
+  board,
+  shape,
+  row,
+  col
+) {
   const newBoard = board.map(r => [...r])
 
   for (let y = 0; y < shape.length; y++) {
     for (let x = 0; x < shape[y].length; x++) {
       if (shape[y][x]) {
         newBoard[row + y][col + x] = 1
-      }
-    }
-  }
-
-  return clearLines(newBoard)
-}
-
-export function clearLines(board) {
-  const newBoard = board.map(r => [...r])
-
-  for (let y = 0; y < 8; y++) {
-    if (newBoard[y].every(cell => cell === 1)) {
-      for (let x = 0; x < 8; x++) {
-        newBoard[y][x] = 0
-      }
-    }
-  }
-
-  for (let x = 0; x < 8; x++) {
-    let full = true
-
-    for (let y = 0; y < 8; y++) {
-      if (!newBoard[y][x]) {
-        full = false
-      }
-    }
-
-    if (full) {
-      for (let y = 0; y < 8; y++) {
-        newBoard[y][x] = 0
       }
     }
   }
